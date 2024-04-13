e9d451a3f51edd724079dcf9f0bb79cc97420c11
OpenAst <isrealme0@gmail.com>
2024-04-12 19:48:49 -0700

    pages and form created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import react from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import NotFound from "./pages/NotFound";
+import Home from './pages/Home';
+import ProtectedRoute from "./components/ProtectedRoute";
+
+const Logout = () => {
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
+const RegisterAndLogout = () => {
+  localStorage.clear()
+  return <Register />
+}
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+
+      return <>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" 
+            element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+            }
+            />
+            <Route 
+              path="/login" element={<Login />}
+            />
+            <Route 
+            path="/register" 
+            element={<RegisterAndLogout />}
+            />
+            <Route 
+              path="*" element={<NotFound />}
+            />
+            <Route 
+              path="/logout" element={<Logout />}
+            />  
+          </Routes>
+        </BrowserRouter>
+      </>
 }
 
 export default App;
